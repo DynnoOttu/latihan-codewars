e57297a45f45af60f8232a479c75cd759e0d12bf
DynnoOttu <dynnoottu968@gmail.com>
2023-08-31 16:50:38 +0800

    merubah tipe data number ke String

---
+++ 9.js
@@ -0,0 +1,16 @@
+// Kita membutuhkan suatu fungsi yang dapat mengubah suatu bilangan (integer) menjadi string.
+
+// Cara apa untuk mencapai hal ini yang Anda ketahui?
+
+// Contoh (masukan --> keluaran):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+}
+
+console.log(numberToString(123));
+console.log(typeof numberToString(123));
